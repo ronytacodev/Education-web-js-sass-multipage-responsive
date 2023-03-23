650df7fac1b3c8c403a52907fc1f5c43ca2138db
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-23 09:03:48 -0500

    use mediaqueries navbar header

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active')
+}
+
+// me quede en el min 18:55
