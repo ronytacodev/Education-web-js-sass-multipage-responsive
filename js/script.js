2f6f3015dce8156adeba5e2098f2aa82acf9641f
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-24 11:33:41 -0500

    add js account-form

--- js/script.js
+++ js/script.js
@@ -7,3 +7,30 @@ document.querySelector('#menu-btn').onclick = () => {
 document.querySelector('#close-navbar').onclick = () => {
     navbar.classList.remove('active');
 }
+
+let registerBtn = document.querySelector('.account-form .register-btn');
+let loginBtn = document.querySelector('.account-form .login-btn');
+
+registerBtn.onclick = () => {
+    registerBtn.classList.add('active');
+    loginBtn.classList.remove('active');
+    document.querySelector('.account-form .login-form').classList.remove('active');
+    document.querySelector('.account-form .register-form').classList.add('active');
+}
+
+loginBtn.onclick = () => {
+    registerBtn.classList.remove('active');
+    loginBtn.classList.add('active');
+    document.querySelector('.account-form .login-form').classList.add('active');
+    document.querySelector('.account-form .register-form').classList.remove('active');
+}
+
+let accountForm = document.querySelector('.account-form');
+
+document.querySelector('#account-btn').onclick = () => {
+    accountForm.classList.toggle('active');
+}
+
+document.querySelector('#close-form').onclick = () => {
+    accountForm.classList.remove('active');
+}
